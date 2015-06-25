4b8d7158e53b0c3c9e383af1f2c12beccfce88dd
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-24 22:21:22 -0400

    Have the soundcloud api working and data displaying from the track i am puling in

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,20 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	Model: function () {
-		SC.initialize({
-		  client_id: 'ed10419c544fca8a3db988fe6d04c020'
-		});
-		      
-		// permalink to a track
-		var track_url = 'https://soundcloud.com/user494758591/matt-and-kim-its-alright';
-		      
-		SC.get('/resolve', { url: track_url }, function(track) {
-		SC.get('/tracks/' + track.id + '/comments', function(comments) {
-		  for (var i = 0; i < comments.length; i++) {
-		    console.log('Someone said: ' + comments[i].body);
-		   }
-		  });
-		});
-	}
+  model: function() {
+    SC.initialize({
+      client_id: "ed10419c544fca8a3db988fe6d04c020"
+    });
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      SC.get('/tracks/35112511', function(song) {
+          resolve(song);
+      });
+    });
+  }
 });
